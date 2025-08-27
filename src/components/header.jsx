f8811b3b2956a7b6d99d31d60f13fe13025e0626
MAS <mhmwdbas310@gmail.com>
2025-08-27 19:34:08 +0300

    edit responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import "../App.css";
 import logo from "../assets/logo.svg";
 import Container from "./container";
-
+import { SlMenu } from "react-icons/sl";
 const Header = () => {
+  const [active, setActive] = useState("free");
   return (
     <>
       <header>
@@ -13,16 +15,55 @@ const Header = () => {
             </div>
 
             <div className="nav">
-              <div>Free designs</div>
-              <div>License</div>
-              <div>Articles</div>
-              <div>Contributors</div>
-              <div>About</div>
+              <div
+                onClick={() => {
+                  setActive("free");
+                }}
+                className={active === "free" ? "active3" : ""}
+              >
+                Free designs
+              </div>
+              <div
+                onClick={() => {
+                  setActive("License");
+                }}
+                className={active === "License" ? "active3" : ""}
+              >
+                License
+              </div>
+              <div
+                onClick={() => {
+                  setActive("Articles");
+                }}
+                className={active === "Articles" ? "active3" : ""}
+              >
+                Articles
+              </div>
+              <div
+                onClick={() => {
+                  setActive("Contributors");
+                }}
+                className={active === "Contributors" ? "active3" : ""}
+              >
+                Contributors
+              </div>
+              <div
+                onClick={() => {
+                  setActive("About");
+                }}
+                className={active === "About" ? "active3" : ""}
+              >
+                About
+              </div>
             </div>
 
             <div className="auth">
               <div>Log in</div>
               <div>Sign Up</div>
+            </div>
+
+            <div className="burger-menu">
+              <SlMenu />
             </div>
           </div>
         </Container>
--- src/components/header2.jsx
+++ src/components/header2.jsx
@@ -4,36 +4,128 @@ import mas from "../assets/mas.svg";
 import box from "../assets/box.svg";
 import xd from "../assets/xd.svg";
 import ps from "../assets/ps.svg";
-
+import { useState } from "react";
 import Container from "./container";
 
 const Header2 = () => {
+  const [active, setActive] = useState("all");
+  const [active1, setActive1] = useState("Latest");
+
+  const [active2, setActive2] = useState("mas");
   return (
     <>
       <div className="header2">
         <Container>
           <div className="content">
+
+            
             <div className="buttons">
-              <div>Latest</div>
-              <div>Popular</div>
+              <div
+                onClick={() => {
+                  setActive1("Latest");
+                }}
+                className={active1 === "Latest" ? "active1" : ""}
+              >
+                Latest
+              </div>
+              <div
+                onClick={() => {
+                  setActive1("Popular");
+                }}
+                className={active1 === "Popular" ? "active1" : ""}
+              >
+                Popular
+              </div>
             </div>
 
             <div className="nav2">
-              <div>
+              <div
+                onClick={() => {
+                  setActive("all");
+                }}
+                className={active === "all" ? "active" : ""}
+              >
                 <p>All</p>
               </div>
-              <div>Web</div>
-              <div>Mobile</div>
-              <div>UI Kit</div>
-              <div>Coded</div>
+              <div
+                onClick={() => {
+                  setActive("web");
+                }}
+                className={active === "web" ? "active" : ""}
+              >
+                <p>Web</p>
+              </div>
+              <div
+                onClick={() => {
+                  setActive("mobile");
+                }}
+                className={active === "mobile" ? "active" : ""}
+              >
+                <p>Mobile</p>
+              </div>
+              <div
+                onClick={() => {
+                  setActive("ui");
+                }}
+                className={active === "ui" ? "active" : ""}
+              >
+                <p>UI Kit</p>
+              </div>
+              <div
+                onClick={() => {
+                  setActive("code");
+                }}
+                className={active === "code" ? "active" : ""}
+              >
+                <p>Coded</p>
+              </div>
             </div>
 
             <div className="logos">
-              <img src={figmaicon} alt="figma" />
-              <img src={mas} alt="mas" />
-              <img src={box} alt="box" />
-              <img src={xd} alt="xs" />
-              <img src={ps} alt="ps" />
+              <div
+                onClick={() => {
+                  setActive2("figma");
+                }}
+                className={active2 === "figma" ? "active2" : ""}
+              >
+                <img src={figmaicon} alt="figma" />
+              </div>
+              <div
+                onClick={() => {
+                  setActive2("mas");
+                }}
+                className={active2 === "mas" ? "active2" : ""}
+              >
+                <img src={mas} alt="mas" />
+              </div>
+
+              <div
+                onClick={() => {
+                  setActive2("box");
+                }}
+                className={active2 === "box" ? "active2" : ""}
+              >
+                {" "}
+                <img src={box} alt="box" />
+              </div>
+              <div
+                onClick={() => {
+                  setActive2("xd");
+                }}
+                className={active2 === "xd" ? "active2" : ""}
+              >
+                {" "}
+                <img src={xd} alt="xs" />
+              </div>
+
+              <div
+                onClick={() => {
+                  setActive2("ps");
+                }}
+                className={active2 === "ps" ? "active2" : ""}
+              >
+                <img src={ps} alt="ps" />
+              </div>
             </div>
           </div>
         </Container>
